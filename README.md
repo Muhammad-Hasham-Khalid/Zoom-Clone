fb504cc4bb337df112149327cd4ee07cc4ebde9a
Muhammad-Hasham-Khalid <m.hasham681@gmail.com>
2020-08-28 01:08:15 +0500

    👍: Done

---
+++ README.md
@@ -0,0 +1,12 @@
+### Install dependencies
+
+- npm i
+
+### Install peer JS
+
+- npm i -g peer
+
+### Run
+
+- npm run dev
+- open in browser : http://localhost:3000
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const uuidv4 = require("uuid").v4;
+const path = require("path");
+const io = require("socket.io")(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(3000, () =>
+  console.log("Server listening: http://localhost:3000")
+);
